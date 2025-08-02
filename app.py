6287925b5345b4051e64ed7c5a776883f1173156
AasrithaNarukulla26 <aasrithachowdary456@gmail.com>
2025-08-02 11:49:58 +0530

    second update

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Welcome to MRECW"
+@app.route("/home")
+def home():
+    return "homme page"
 if __name__ == "_main_":
     app.run(host = '0.0.0',port=10000)
